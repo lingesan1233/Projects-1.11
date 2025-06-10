8696ee3edad5ec0dcfaae6583df85600ff9b6248
lingesan <sglingesan@gmail.com>
2025-06-10 08:04:13 +0530

    f31

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 // ✅ CORS Setup
 const allowedOrigins = [
   //'https://6847394284fffc11d9715017--calm-froyo-09c0c1.netlify.app', // Netlify Preview
-  'https://stellular-babka-06b120.netlify.app' // Netlify Production
+  'https://snazzy-babka-5cdb70.netlify.app' // Netlify Production
 ];
 
 const corsOptions = {
