83c86cc0353382d770931cfd1755259f8ce5407c
lingesan <sglingesan@gmail.com>
2025-06-10 07:09:42 +0530

    f22

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { FaPenAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+	return (
+				<div className="flex flex-col min-w-[280px] w-[33%] my-3">
+					<h1 className="font-semibold mb-2">Pages</h1>
+					<span>
+						<Link
+							className="hover:text-blue-600 hover:underline"
+							to={"/"}
+						>
+							Chat App
+						</Link>
+					</span>
+					<span>
+						<Link
+							className="hover:text-blue-600 hover:underline"
+							to={"/signin"}
+						>
+							SignIn
+						</Link>
+					</span>
+					<span>
+						<Link
+							className="hover:text-blue-600 hover:underline"
+							to={"/signup"}
+						>
+							SignUp
+						</Link>
+					</span>
+					<span>
+						<Link
+							className="hover:text-blue-600 hover:underline"
+							to={"/home"}
+						>
+							Home
+						</Link>
+					</span>
+				</div>
+	);
+};
+
+export default Footer;
